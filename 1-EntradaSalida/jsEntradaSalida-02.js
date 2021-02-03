21acf0ee0f9026031ee95c70bda445d0682102ab
Santiago Prol <sprol@bagnols_sa>
2021-02-03 14:05:05 -0300

    segundo ejercicio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,9 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	
+	var nombre = prompt("ingresar dato");
+	document.write("el dato ingresado es" + nombre)
+	alert("nombre");
 }
 
